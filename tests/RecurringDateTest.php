0ef008a5f611f4d131935bb41476f109e4a260d3
Roberto Silva <roberto.silva@arcasolutions.com>
2017-06-06 09:11:55 -0300

    Adds more tests

--- tests/RecurringDateTest.php
+++ tests/RecurringDateTest.php
@@ -29,4 +29,28 @@ final class RecurringDateTest extends TestCase {
         $this->assertEquals('2017-08-08', $next);
     }
 
+    public function testShouldReturnNextMothersDay() {
+        // Given
+        $date = "2017-05-14"; // mother's day
+        $rrule = "FREQ=YEARLY;BYMONTH=5;BYDAY=2SU";
+
+        // When
+        $next = RecurringDate::getNextOccurence($date, $rrule);
+
+        // Then
+        $this->assertEquals('2018-05-13', $next);
+    }
+
+    public function testShouldReturnNextArcaBirthdayParty() {
+        // Given
+        $date = "2017-01-27"; // arca birthday party
+        $rrule = "FREQ=MONTHLY;BYDAY=-1FR";
+
+        // When
+        $next = RecurringDate::getNextOccurence($date, $rrule);
+
+        // Then
+        $this->assertEquals('2017-06-30', $next);
+    }
+
 }
